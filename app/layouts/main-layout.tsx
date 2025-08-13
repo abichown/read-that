962092b747048ba31d985bf3b000590a95469ea5
Abi Chown <abi.chown7@gmail.com>
2025-08-13 16:38:41 +0100

    Move MetaArgs to MainLayout

--- app/layouts/main-layout.tsx
+++ app/layouts/main-layout.tsx
@@ -1,6 +1,18 @@
 import { Outlet } from 'react-router';
+import type { Route } from './+types/main-layout';
+
 import Header from '../components/header';
 import Footer from '../components/footer';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Read that!' },
+    {
+      name: 'description',
+      content: 'Welcome to Read that! A reading tracker.',
+    },
+  ];
+}
 
 const MainLayout = () => (
   <>
